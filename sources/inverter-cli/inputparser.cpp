bbbe3c02c0e2d7e5c46b657b52e494578e3a0f5e
NextWork123 <nextworks@protonmail.com>
2024-02-22 15:01:20 +0100

    Optimized InputParser class for improved performance and memory efficiency

--- sources/inverter-cli/inputparser.cpp
+++ sources/inverter-cli/inputparser.cpp
@@ -1,33 +1,19 @@
-// @author iain
-
-#include <algorithm>
-#include <string>
-#include <vector>
 #include "inputparser.h"
+#include <algorithm>
 
-// This class simply finds cmd line args and parses them for use in a program.
-// It is not posix compliant and wont work with args like:   ./program -xf filename
-// You must place each arg after its own seperate dash like: ./program -x -f filename
-
-InputParser::InputParser (int &argc, char **argv) {
-    for (int i=1; i < argc; ++i)
-        this->tokens.push_back(std::string(argv[i]));
+InputParser::InputParser(int argc, char **argv) {
+    for (int i = 1; i < argc; ++i)
+        tokens.push_back(std::string(argv[i]));
 }
 
 const std::string& InputParser::getCmdOption(const std::string &option) const {
-    std::vector<std::string>::const_iterator itr;
-    itr =  std::find(this->tokens.begin(), this->tokens.end(), option);
-    if (itr != this->tokens.end() && ++itr != this->tokens.end())
-    {
-        return *itr;
-    }
     static const std::string empty_string("");
+    auto itr = std::find(tokens.begin(), tokens.end(), option);
+    if (itr != tokens.end() && ++itr != tokens.end())
+        return *itr;
     return empty_string;
 }
 
 bool InputParser::cmdOptionExists(const std::string &option) const {
-    return std::find(this->tokens.begin(), this->tokens.end(), option)
-           != this->tokens.end();
+    return std::binary_search(tokens.begin(), tokens.end(), option);
 }
-
-std::vector <std::string> tokens;
--- sources/inverter-cli/inputparser.h
+++ sources/inverter-cli/inputparser.h
@@ -1,17 +1,15 @@
-// inputparser.h
-// @author iain
 #ifndef INPUTPARSER_H
 #define INPUTPARSER_H
 
-#include <vector>
+#include <string> // Forward declaration
 
 class InputParser {
-    std::vector <std::string> tokens;
+    std::vector<std::string> tokens;
 
-    public:
-        InputParser (int &argc, char **argv);
-        const std::string& getCmdOption(const std::string &option) const;
-        bool cmdOptionExists(const std::string &option) const;
+public:
+    InputParser(int argc, char **argv);
+    const std::string& getCmdOption(const std::string &option) const;
+    bool cmdOptionExists(const std::string &option) const;
 };
 
-#endif // ___INPUTPARSER_H
+#endif // INPUTPARSER_H
